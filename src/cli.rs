5fe4e436466cbf117bfdb4c17630a31ac3bce7e0
rcalimlim <ross.calimlim@gmail.com>
2023-12-21 18:20:19 -0500

    poc

--- src/cli.rs
+++
@@ -1,9 +0,0 @@
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-pub struct Args {
-    // name of the gitignore file keyword to get
-    #[arg(short, long)]
-    keyword: String,
-}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
-mod cli;
-
-use clap::*;
-use cli::Args;
+use clap::{arg, Command};
+use std::{fs::File, io::Write};
 
 fn main() {
-    let args = Args::parse();
-    println!("{:?}", args);
+    const API_URL: &str = "https://www.toptal.com/developers/gitignore/api";
+
+    let cmd = Command::new("gi")
+        .arg(arg!([keywords] "Comma separated keywords to generate a .gitignore for."))
+        .get_matches();
+
+    if let Some(keywords) = cmd.get_one::<String>("keywords") {
+        let result = reqwest::blocking::get(format!("{}/{}", API_URL, keywords));
+
+        if let Ok(res) = result {
+            if let Ok(mut file) = File::create(".gitignore") {
+                if let Ok(bytes) = &res.bytes() {
+                    let write_res = file.write(&bytes);
+                    match write_res {
+                        Ok(_) => println!("Successfully create .gitignore for {}", keywords),
+                        Err(err) => println!("Error: {}", err),
+                    }
+                } else {
+                    println!("Could not get .gitignore for arg `{}`", keywords);
+                }
+            }
+        } else {
+            println!("Could not get .gitignore for arg `{}`", keywords);
+        }
+    }
 }
